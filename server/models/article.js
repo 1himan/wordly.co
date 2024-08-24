fa15ad9e4f2d2c2fc594b9004d1bc2c99999b873
Himanshu <140396762+1himan@users.noreply.github.com>
2024-08-24 13:26:31 +0530

    Some Minor Updates

--- server/models/article.js
+++ server/models/article.js
@@ -7,6 +7,7 @@ const articleSchema = new mongoose.Schema(
       required: true,
       default: "Please Write A Heading For This article",
     },
+    readTime: { type: Number, default: 0 },
     description: {
       type: String,
       default:
@@ -19,11 +20,19 @@ const articleSchema = new mongoose.Schema(
       required: true,
     },
     tags: { type: [String], default: ["tag1, tag2"] },
+    views: { type: Number, default: 0 },
     status: { type: String, enum: ["draft", "published"], default: "draft" },
     date: { type: Date, default: Date.now }, // Add date field with default value
     likeCount: { type: Number, default: 0 }, // Add likeCount field with default value
     commentCount: { type: Number, default: 0 }, // Add commentCount field with default value
-    cover: { type: String , default:""}, // Add cover field to store URL or path to cover image
+    cover: { type: String, default: "" }, // Add cover field to store URL or path to cover image
+    comments: [
+      {
+        user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+        content: { type: String },
+        date: { type: Date, default: Date.now },
+      },
+    ],
   },
   {
     strict: false, // Allow schema-less storage for blocks
--- server/models/writer.js
+++ server/models/writer.js
@@ -5,6 +5,14 @@ const writerSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   articles: [{ type: mongoose.Schema.Types.ObjectId, ref: "Article" }],
+  bio: { type: String }, 
+  profilePicture: { type: String }, 
+  socialMedia: {
+    twitter: { type: String }, 
+    linkedin: { type: String }, 
+    facebook: { type: String }, 
+    instagram: { type: String }, 
+  },
 });
 
 export const Writer = mongoose.model("Writer", writerSchema);
